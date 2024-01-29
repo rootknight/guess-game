9572972256e8c12f0aded55fbe44a2442ef5cd67
rootknight <ljmroot@gmail.com>
2024-01-30 02:27:27 +0800

    使用Drizzle，完成fetchdata

--- app/admin/(dashboard)/categories/page.tsx
+++ app/admin/(dashboard)/categories/page.tsx
@@ -15,7 +15,6 @@ const Page = async ({
 }) => {
   const query = searchParams?.query || "";
   const categories = await fetchCategories(query);
-  console.log(categories);
 
   return (
     <div className="flex flex-col gap-4">
