a09992ce31fc1756df70f51331495fbce6fb60ac
rootknight <ljmroot@gmail.com>
2024-02-05 01:21:13 +0800

    删除githubprovider

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import Credentials from "next-auth/providers/credentials";
-import GitHub from "next-auth/providers/GitHub";
 import { z } from "zod";
 import bcrypt from "bcryptjs";
 import { getUser } from "@/lib/fetchers/getUser";
