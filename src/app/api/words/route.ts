fde21bb51c299da7dde504c2344c128503ca05b8
rootknight <ljmroot@gmail.com>
2024-02-17 17:06:37 +0800

    修正依赖

--- src/app/api/words/route.ts
+++
@@ -1,8 +0,0 @@
-import { getWordsByCategory } from "@/lib/fetchers/data";
-
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const type = searchParams.get("type") || "";
-  const res = await getWordsByCategory(type);
-  return Response.json({ res });
-}
